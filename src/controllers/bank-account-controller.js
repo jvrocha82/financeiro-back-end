afa80798ba2b2a7ed9945a623db07ded8d527548
jvrocha82 <jvrocha82@gmail.com>
2021-04-19 11:24:58 -0300

    ajustes de formatação e ajustes do adição do cors

--- src/controllers/bank-account-controller.js
+++ src/controllers/bank-account-controller.js
@@ -1,40 +1,44 @@
-const bankAccountModel = require('../models/bank-account-model');
-const {createBankAccount,updateBankAccount,deleteBankAccount,findBankAccount,listBankAccount } = require('../business/bank-account-business')
+const bankAccountModel = require("../models/bank-account-model");
+const {
+  createBankAccount,
+  updateBankAccount,
+  deleteBankAccount,
+  findBankAccount,
+  listBankAccount,
+} = require("../business/bank-account-business");
 class bankAccountController {
+  async create(req, res) {
+    const currenctBankAccount = req.body;
+    await createBankAccount(currenctBankAccount)
+      .then((response) => res.status(200).json(response))
+      .catch((error) => res.status(500).json(error));
+  }
 
-    async create(req, res) {
-        const currenctBankAccount = req.body;
-        await createBankAccount(currenctBankAccount)
-            .then(response => res.status(200).json(response))
-            .catch(error => res.status(500).json(error))
-    }
+  async list(req, res) {
+    await listBankAccount()
+      .then((response) => res.status(200).json(response))
+      .catch((error) => res.status(500).json(error));
+  }
 
-    async list(req, res) {
-        await listBankAccount()
-            .then(response => res.status(200).json(response))
-            .catch(error => res.status(500).json(error))
-    }
-
-    async find(req, res) {
-        const bankAccountId = req.params.id;
-        await findBankAccount(bankAccountId)
-            .then(response => res.status(200).json(response))
-            .catch(error => res.status(500).json(error))
-    }
-
-    async delete(req, res) {
+  async find(req, res) {
     const bankAccountId = req.params.id;
-      await deleteBankAccount(bankAccountId)
-      
-            .then(() => res.status(200).json('Conta bancária deletada'))
-            .catch(error => res.status(500).json(error))
-    }
-    async update(req, res) {
-        const currentBankAccount = req.body;
-        const bankAccountId = req.params.id;
-        await updateBankAccount(currentBankAccount,bankAccountId)
-            .then(response => res.status(200).json(response))
-            .catch(error => res.status(500).json(error))
-    }
+    await findBankAccount(bankAccountId)
+      .then((response) => res.status(200).json(response))
+      .catch((error) => res.status(500).json(error));
+  }
+
+  async delete(req, res) {
+    const bankAccountId = req.params.id;
+    await deleteBankAccount(bankAccountId)
+      .then(() => res.status(200).json("Conta bancária deletada"))
+      .catch((error) => res.status(500).json(error));
+  }
+  async update(req, res) {
+    const currentBankAccount = req.body;
+    const bankAccountId = req.params.id;
+    await updateBankAccount(currentBankAccount, bankAccountId)
+      .then((response) => res.status(200).json(response))
+      .catch((error) => res.status(500).json(error));
+  }
 }
 module.exports = new bankAccountController();
--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const server = express();
+const cors = require('cors')
+
 require('dotenv').config()
 server.use(express.json());
-
+server.use(cors()) 
+var helmet = require("helmet")
 const accountRoutes = require('./routes/routes');
 server.use('/', accountRoutes);
 
-server.listen(3000, () => {
+
+
+
+server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+  });
+
+
+server.listen(3001, () => {
     console.log('api online')
 })
